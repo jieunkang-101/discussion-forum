8e4a8e97f5dbb33ebeddc39261d6552d05823167
Jieun Kang <jieunkang101@gmail.com>
2020-04-28 11:45:25 -0700

    add and pass the 2nd test for adding post

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,42 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+
+  let action;
+  const postData = {
+    author: 'Claire',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    tags: 'magic',
+    likes: 0,
+    timeStamp: 'Tue Apr 28 2020 11:30:26 GMT-0700 (Pacific Daylight Time)',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new post data to masterPostList', () => {
+    const { author, content, tags, likes, timeStamp, id } = postData;
+    action = {
+      type: 'ADD_POST',
+      author: author,
+      content: content,
+      tags: tags,
+      likes: likes,
+      timeStamp: timeStamp,
+      id: id
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        author: author,
+        content: content,
+        tags: tags,
+        likes: likes,
+        timeStamp: timeStamp,
+        id: id
+      }
+    });
+  });  
+
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,21 @@
 export default (state = {}, action) => {
-  return state;
+  const { author, content, tags, likes, timeStamp, id } = action;
+  switch (action.type) {
+  case 'ADD_POST':
+    return Object.assign({}, state, {
+      [id]: {
+        author: author,
+        content: content,
+        tags: tags,
+        likes: likes,
+        timeStamp: timeStamp,
+        id: id
+      }
+    });  
+  
+
+
+  default:
+    return state;
+  }
 };
